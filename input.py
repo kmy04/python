9728a5f463b53e68497428920f80065d433ea092
kmy04 <biie2004@naver.com>
2025-08-19 21:56:10 +0900

    입력받기 예제

---
+++ input.py
@@ -0,0 +1,18 @@
+name = input("이름을 입력하세요: ")
+print("안녕하세요, " + name + "님!")
+print(f"안녕하세요, {name}님!")
+
+# input의 반환형은 항상 string이다.
+# 그렇기 때문에 숫자로 받고 싶으면 타입 캐스팅을 해줘야한다.
+try:
+    number = int(input("숫자를 입력하세요: "))
+
+    if number % 2 == 0:
+        print(f"{number}는 짝수입니다.")
+    else:
+        print(f"{number}는 홀수입니다.")
+
+# ValueError는 문자열을 숫자로 바꾸려 할 때 문자열이
+# 올바른 숫자가 아닐 경우 등 값 자체가 유효하지 않을 때 발생한다
+except ValueError:
+    print("숫자를 입력해야 합니다.")
